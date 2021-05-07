e13a9dfaa128e9e686d62deebca388271a0f936d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-07 13:58:20 +0200

    kosko

---
+++ kosko/components/nginx.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { Deployment } = require("kubernetes-models/apps/v1/Deployment");
+const { Service } = require("kubernetes-models/v1/Service");
+
+const metadata = { name: "nginx", namespace: "sample-next-app" };
+const labels = { app: "nginx" };
+
+const deployment = new Deployment({
+  metadata,
+  spec: {
+    replicas: 1,
+    selector: {
+      matchLabels: labels,
+    },
+    template: {
+      metadata: {
+        labels,
+      },
+      spec: {
+        containers: [
+          {
+            image: "nginx",
+            name: "nginx",
+            ports: [
+              {
+                containerPort: 80,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
+});
+
+const service = new Service({
+  metadata,
+  spec: {
+    selector: labels,
+    type: "ClusterIP",
+    ports: [
+      {
+        port: 80,
+        targetPort: 80,
+      },
+    ],
+  },
+});
+
+module.exports = [deployment, service];
